3459f3dcb767b27995e8b66ac4997d070a276df4
Vincent Crowe <vinla@hotmail.co.uk>
2018-01-09 16:31:11 +0000

    Image picker control

---
+++ cgwo/Controls/ImagePicker.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cgwo.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImagePicker.xaml
+    /// </summary>
+    public partial class ImagePicker : UserControl
+    {
+        public ImagePicker()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(nameof(ImageSource), typeof(ImageSource), typeof(ImagePicker));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        private void ChooseImageClick(object sender, RoutedEventArgs e)
+        {
+            using (var openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;*.gif";
+                if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.UriSource = new Uri(openFileDialog.FileName, UriKind.Absolute);
+                    image.EndInit();
+                    ImageSource = image;
+                }
+            }
+        }
+    }
+}
