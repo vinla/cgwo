cdd4a8a6b1814dfcca606646e338c0b45e74156a
Vincent Crowe <vinla@hotmail.co.uk>
2018-02-19 15:02:06 +0000

    Fixes for copy and paste functionality

--- cgwo/Designer/CardElementClipboard.cs
+++ cgwo/Designer/CardElementClipboard.cs
@@ -1,21 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Cogs.Designer
 {
 	public static class CardElementClipboard
 	{
-		private static readonly List<CardElement> _elements = new List<CardElement>();
-
+		private static List<JsonObject> _objects;
 		public static void SetElements(IEnumerable<CardElement> elements)
 		{
-			_elements.Clear();
-			_elements.AddRange(elements.Select(el => el.Clone(false)).Cast<CardElement>());
+			_objects = elements.Select(el => JsonObject.Serialize(el)).ToList();
 		}
 
 		public static IEnumerable<CardElement> GetElements()
 		{
-			return _elements.Select(el => el.Clone(false)).Cast<CardElement>();
+			return _objects.Select(obj => obj.Deserialize()).Cast<CardElement>();
+		}
+	}
+
+	public class JsonObject
+	{
+		private readonly Type _type;
+		private readonly string _json;
+
+		private JsonObject(Type type, string json)
+		{
+			_type = type;
+			_json = json;
+		}
+
+		public static JsonObject Serialize(object obj)
+		{
+			var json = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+			return new JsonObject(obj.GetType(), json);
+		}
+
+		public object Deserialize()
+		{
+			return Newtonsoft.Json.JsonConvert.DeserializeObject(_json, _type);
 		}
 	}
 }
